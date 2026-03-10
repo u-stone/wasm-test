49dadb62d4d1837980b336340c5d982ffc5ea073
liuguoyuan <guoyuan@gritworld.com>
2026-03-10 17:14:41 +0800

    move cmake demo build flags into cmake script

--- demos/05-cmake-emcmake/build.ps1
+++ demos/05-cmake-emcmake/build.ps1
@@ -22,13 +22,7 @@ try {
     $buildDir = Join-Path 'build' $mode
     New-Item -ItemType Directory -Force -Path $outDir | Out-Null
 
-    $debugFlags = switch ($mode) {
-      'release' { '-O2' }
-      'dwarf' { '-O0 -g' }
-      'sourcemap' { '-O1 -gsource-map' }
-    }
-
-    & emcmake cmake -S . -B $buildDir "-DWASM_DEBUG_MODE=$mode" "-DWASM_DEBUG_FLAGS=$debugFlags"
+    & emcmake cmake -S . -B $buildDir "-DWASM_DEBUG_MODE=$mode"
     & cmake --build $buildDir --target cmake_demo
 
     Write-Host "Built demos/05-cmake-emcmake/output/$mode/cmake_demo.js"
